63d2bbe781f92032c4a9b69123ace61816d9dd0c
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-08-18 12:38:55 +0100

    First commit

---
+++ src/main/java/com/interswitch/test/App.java
@@ -0,0 +1,11 @@
+package com.interswitch.test;
+
+import com.interswitch.maven.ArithmeticOperations;
+
+public class App
+{
+  public static void main( String[] args )
+  {
+    ArithmeticOperations.userOperation();
+  }
+}
