1e7e3833964122d55bc553dff6714efb0b3f8e3f
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-08-18 16:10:06 +0100

    Go home print out

--- src/main/java/com/interswitch/test/App.java
+++ src/main/java/com/interswitch/test/App.java
@@ -2,6 +2,8 @@ package com.interswitch.test;
 
 import com.interswitch.maven.ArithmeticOperations;
 
+import java.time.LocalDateTime;
+
 public class App
 {
   public static void main( String[] args )
@@ -12,5 +14,7 @@ public class App
     System.out.println(upperLowerCase.toUpperCase("Tgjagjkk hskjhkaj"));
     System.out.println(upperLowerCase.toLowerCase("Tgjagjkk hskjhkaj"));
     System.out.println(upperLowerCase.toLowerCase(""));
+
+    System.out.println("Please, Let's go home! " + LocalDateTime.now());
   }
 }
